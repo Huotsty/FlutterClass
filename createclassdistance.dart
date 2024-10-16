e5908b9f70f85cce3c02e25484258cfac823f4c2
Huotsty <122008167+Huotsty@users.noreply.github.com>
2024-10-16 22:05:43 +0700

    createclassdistance

---
+++ createclassdistance.dart
@@ -0,0 +1,61 @@
+enum DistanceUnit {
+   kilometers,
+   meters,
+   centimeters
+}
+class Distance {
+  final double _distance;
+  final DistanceUnit _unit;
+  
+  Distance.kms(double kilometers):
+    _distance = kilometers, 
+    _unit = DistanceUnit.kilometers;
+
+  Distance.ms(double meters): 
+    _distance = meters, 
+    _unit = DistanceUnit.meters;
+
+  Distance.cms(double centimeters):
+    _distance = centimeters,
+    _unit = DistanceUnit.centimeters;
+
+  double get distance => _distance;
+  DistanceUnit get unit => _unit;
+
+  double _convertToMeters() {
+    switch (_unit) {
+      case DistanceUnit.kilometers:
+        return _distance * 1000;
+      case DistanceUnit.meters:
+        return _distance;
+      case DistanceUnit.centimeters:
+        return _distance / 100;
+    }
+  }
+  double toKilometers() {
+  return _convertToMeters() / 1000;
+  }
+
+  double toCentimeters(){
+    return _convertToMeters() * 100;
+  }
+
+  Distance operator + (Distance other) {
+    return Distance.ms(this._convertToMeters() + other._convertToMeters()); 
+  }
+  
+    @override
+  String toString() {
+    return '$_distance ${_unit.name}';
+  }
+}
+void main() {
+  Distance d1 = Distance.kms(5);  // 5 kilometers
+  Distance d2 = Distance.ms(500); 
+   Distance d3 = Distance.cms(500);// 500 meters
+  Distance sum = d1 + d2 + d3;
+  print('Sum: $sum');
+  print('sum:  ${sum.toKilometers()} kms');
+  print('sum: ${sum.toCentimeters()} cms');
+  
+}
