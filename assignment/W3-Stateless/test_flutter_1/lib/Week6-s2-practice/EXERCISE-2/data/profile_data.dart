3b05e8f9c1d920d57762a4a4a4f9c2e40bed5e52
Huotsty <122008167+Huotsty@users.noreply.github.com>
2024-11-14 09:30:47 +0700

    add

---
+++ assignment/W3-Stateless/test_flutter_1/lib/Week6-s2-practice/EXERCISE-2/data/profile_data.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:test_flutter_1/Week6-s2-practice/EXERCISE-2/model/profile_tile_model.dart';
+
+ 
+ProfileData ronanProfile = ProfileData(
+    name: "Ronan",
+    position: "Flutter Developer",
+    avatarUrl: 'assets/w5-s2/aang.png',
+    tiles: [
+      TileData( icon: Icons.phone, title: "Phone Number", value: "+123 456 7890"),
+      TileData(icon: Icons.location_on, title: "Address", value: "123 Cambodia"),
+      TileData(icon: Icons.email, title: "Mail", value: "ronan.ogogr@cadt.edu"),
+    ]);
