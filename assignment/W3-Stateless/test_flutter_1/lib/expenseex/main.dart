a89e6a04f56972c8b000791d864680116447eb60
Huotsty <122008167+Huotsty@users.noreply.github.com>
2024-11-26 23:26:42 +0700

    expense

---
+++ assignment/W3-Stateless/test_flutter_1/lib/expenseex/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'screens/expenses.dart';
+
+
+ 
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    ),
+  );
+}
