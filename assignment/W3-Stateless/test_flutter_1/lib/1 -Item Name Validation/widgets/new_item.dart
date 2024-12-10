3980d3d480d8af72c842406344a5f47c7f463880
Huotsty <122008167+Huotsty@users.noreply.github.com>
2024-12-10 07:34:07 +0700

    challenge

--- assignment/W3-Stateless/test_flutter_1/lib/1 -Item Name Validation/widgets/new_item.dart
+++ assignment/W3-Stateless/test_flutter_1/lib/1 -Item Name Validation/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../models/grocery_category.dart';
 
 class NewItem extends StatefulWidget {
@@ -11,7 +12,65 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  // We create a key to access and control the state of the Form.
+  final _formKey = GlobalKey<FormState>();
+
+  String _enteredName = '';
+  int _enteredQuantity = 0;
+
+  void _saveItem() {
+    // 1 - Validate the form
+    bool isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      // 2 - Save the form to get last entered values
+      _formKey.currentState!.save();
+
+      // TODO: Get the last entered quantity
+      print("Name $_enteredName\nQuantity $_enteredQuantity");
+    }
+  }
+
+  void _resetForm() {
+    // TODO: reset the form
+  }
+
+  String? validateTitle(String? value) {
+    if (value == null ||
+        value.isEmpty ||
+        value.trim().length <= 1 ||
+        value.trim().length > 50) {
+      return 'Must be between 1 and 50 characters.';
+    }
+    return null;
+  }
+
+  // TODO: validate quantity
+  String? validateQuantity(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Quantity is required';
+    }
+    final quantity = int.tryParse(value);
+    if (quantity == null || quantity <= 0) {
+      return 'Please enter a valid, positive number';
+    }
+    return null;
+  }
+
+  // String? validateQuantity(String? value) {
+  //   String errorMessage = '';
+  //   if (value == null || value.isEmpty) {
+  //     print('Hello');
+  //     errorMessage = 'Must be a valid';
+  //     if (int.tryParse(value!)! < 0) {
+  //       errorMessage = '$errorMessage, positive number';
+  //       return errorMessage;
+  //     } else {
+  //       return errorMessage;
+  //     }
+  //   }
+  //   return null;
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,19 +82,16 @@ class _NewItemState extends State<NewItem> {
         child: Form(
           key: _formKey,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               TextFormField(
                 maxLength: 50,
                 decoration: const InputDecoration(
                   label: Text('Name'),
                 ),
-                validator: (String? value) {
-            if (value == null || value.isEmpty) {
-              return 'Please enter name';
-            }
-            return null;
-          },
+                validator: validateTitle,
+                onSaved: (value) {
+                  _enteredName = value!;
+                },
               ),
               const SizedBox(height: 10),
               Row(
@@ -47,10 +103,9 @@ class _NewItemState extends State<NewItem> {
                         label: Text('Quantity'),
                       ),
                       initialValue: '1',
-                      keyboardType: TextInputType.number,
-                      validator: (value){
-                        
-                      } ,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      validator: validateQuantity,
+                      onSaved: (value) => _enteredQuantity = int.parse(value!),
                     ),
                   ),
                   const SizedBox(width: 8),
@@ -83,16 +138,11 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () => {},
+                    onPressed: _resetForm,
                     child: const Text('Reset'),
                   ),
                   ElevatedButton(
-                    onPressed: () => {
-                      if (_formKey.currentState!.validate()) {
-                        // Handle form submission
-                        _formKey.currentState!.reset()
-                      }
-                    },
+                    onPressed: _saveItem,
                     child: const Text('Add Item'),
                   )
                 ],
